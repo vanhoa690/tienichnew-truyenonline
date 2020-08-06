678e8d4e0d87ad954f0ab273f0cff1da426048ed
vanhoa690 <68044489+vanhoa690@users.noreply.github.com>
2020-08-06 22:36:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ function removeAccents(str) {
   let content = str.normalize('NFD')
             .replace(/[\u0300-\u036f]/g, '')
             .replace(/đ/g, 'd').replace(/Đ/g, 'D');
-    let result = content.replace(/ +/g, '-').toLowerCase();
+   let result = content.replace(/ +/g, '');
+//     let result = content.replace(/ +/g, '-').toLowerCase();
   // let result = content.split(" ").join('-').toLowerCase();
    console.log(result);
   show.innerText = result;
