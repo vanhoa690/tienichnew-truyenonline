0e8def5ee3f257cb27b021e8557f7a352c4e019f
vanhoa690 <68044489+vanhoa690@users.noreply.github.com>
2020-08-06 22:32:49 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+let tx = document.getElementById('tx');
+let btn = document.getElementById('btn');
+let show = document.getElementById('show');
+btn.addEventListener('click', () => removeAccents(tx.value));
+
+function removeAccents(str) {
+  str = str.trim();
+  // str = str.trim().replace(/\s/g, ' ');
+  let content = str.normalize('NFD')
+            .replace(/[\u0300-\u036f]/g, '')
+            .replace(/đ/g, 'd').replace(/Đ/g, 'D');
+    let result = content.replace(/ +/g, '-').toLowerCase();
+  // let result = content.split(" ").join('-').toLowerCase();
+   console.log(result);
+  show.innerText = result;
+}
